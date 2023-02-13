2f13350a59ff77f466125c29e7b83fb3291a18f3
VazhnovPavel <pavel.fortex@yandex.ru>
2023-02-13 14:20:13 +0200

    Первая загрузка
    тест

---
+++ CRUDBackend/Program.cs
@@ -0,0 +1,40 @@
+﻿using CRUDBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddDbContext<EmployeeContext>(options
+    => options.UseNpgsql(builder.Configuration.GetConnectionString("CRUDCS")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
